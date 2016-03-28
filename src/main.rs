474da2fd80c827a31e276b28938ad30537e47350
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-03-28 20:42:40 +0200

    Yaml parser PoC

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,111 @@ use std::collections::HashMap;
 use std::io::prelude::*;
 use std::fs::File;
 use std::net::TcpStream;
-use yaml_rust::{YamlLoader};
+use yaml_rust::{YamlLoader, Yaml};
 
 type Kwargs<'a> = HashMap<&'a str, &'a str>;
+
+#[derive(Debug)]
+struct Property {
+    name: String,
+    module: String,
+    params: HashMap<String, String>
+}
+
+#[derive(Debug)]
+struct DuckCheck<'a> {
+    hosts: &'a Vec<String>,
+    properties: Vec<Property>
+}
 
 fn main() {
     let mut f = File::open("./examples/esel.ducks").unwrap();
     let mut yaml_str = String::new();
     let _ = f.read_to_string(&mut yaml_str);
     let docs = YamlLoader::load_from_str(&yaml_str).unwrap();
-
     println!("* {:?}", docs);
+
+/*
+[Array([
+       Hash({
+           String("inventory"): Hash({
+               String("raspi"): Array([
+                    String("heimbot.fritz.box")])})}),
+       Hash({
+           String("hosts"): String("raspi"),
+           String("properties"): Array([
+             Hash({
+                 String("name"): String("Checking SSH"),
+                 String("ssh"): Hash({
+                     String("port"): Integer(22),
+                     String("software"): String("OpenSSH.*"),
+                     String("version"): Real("2.0")})})])})])]
+*/
+
+    // We assume only one document in the file which consists of a list of hashes
+    let mut inventory: HashMap<String, Vec<String>> = HashMap::new();
+    let INVENTORY = Yaml::from_str("inventory");
+    let HOSTS = Yaml::from_str("hosts");
+    let PROPERTIES = Yaml::from_str("properties");
+    let NAME = Yaml::from_str("name");
+    let mut duck_checks = Vec::new();
+    for hash in docs[0].as_vec().unwrap() {
+        let map = hash.as_hash().unwrap();
+        if map.contains_key(&INVENTORY) {
+            println!("- Found inventory: {:?}", hash);
+            let inventory_yaml = map.get(&INVENTORY).unwrap().as_hash().unwrap();
+            for hosts_name_yaml in inventory_yaml.keys() {
+                let hosts_name = hosts_name_yaml.as_str().unwrap().to_string();
+                let mut hosts: Vec<String> = Vec::new();
+                for host in inventory_yaml.values().next().unwrap().as_vec().unwrap() {
+                    let h = host.as_str().unwrap().to_string();
+                    hosts.push(h);
+                }
+                println!("- - Inventory name: '{:?}'", hosts_name);
+                println!("- - Inventory hosts: '{:?}'", hosts);
+                inventory.insert(hosts_name.to_string(), hosts);
+            }
+        }
+    }
+    for hash in docs[0].as_vec().unwrap() {
+        let map = hash.as_hash().unwrap();
+        if map.contains_key(&INVENTORY) {
+        } else {
+            if map.contains_key(&HOSTS) && map.contains_key(&PROPERTIES) {
+                println!("- Found duck check: {:?}", hash);
+                let hosts = inventory.get(map.get(&HOSTS).unwrap().as_str().unwrap()).unwrap();
+                let mut properties = Vec::new();
+
+                let properties_yaml = map.get(&PROPERTIES).unwrap().as_vec().unwrap();
+                for property_yml in properties_yaml {
+                    let mut name: Option<String> = None;
+                    let mut params = HashMap::new();
+                    let mut module: Option<String> = None;
+                    for elem in property_yml.as_hash().unwrap() {
+                        if elem.0 == &NAME {
+                            name = Some(elem.0.as_str().unwrap().to_string());
+                        } else {
+                            module = Some(elem.0.as_str().unwrap().to_string());
+                        }
+                    }
+                    if module.is_some() {
+                        let params_yaml = property_yml.as_hash().unwrap().get(&Yaml::from_str(module.as_ref().unwrap())).unwrap().as_hash().unwrap();
+                        for kv in params_yaml {
+                            params.insert(kv.0.as_str().unwrap().to_string(), kv.0.as_str().unwrap().to_string());
+                        }
+                    }
+                    properties.push( Property { name: name.unwrap(), module: module.unwrap(), params: params } );
+                }
+
+                let duck_check = DuckCheck { hosts: hosts, properties: properties };
+                println!("- Created a duck check: {:?}", duck_check);
+                duck_checks.push(duck_check);
+            }
+
+        }
+    }
+    println!("* Inventory: {:?}", inventory);
+    println!("* DuckChecks: {:?}", duck_checks);
 
     let gil = Python::acquire_gil();
     let py = gil.python();
