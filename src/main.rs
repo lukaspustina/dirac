23bf04866f2f7de5ec696722a55f6b840b1317db
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-04-05 16:06:10 +0200

    Adds arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
@@ -5,6 +6,7 @@ extern crate term_painter;
 
 extern crate dirac;
 
+use clap::{Arg, ArgMatches, App};
 use std::collections::HashMap;
 use std::env;
 use term_painter::ToStyle;
@@ -13,16 +15,29 @@ use term_painter::Attr::*;
 
 use dirac::checks::CheckSuite;
 
-fn main() {
-    // PYTHONPATH=modules cargo run -- examples/pdt.yml
+static VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+fn main() {
     if env_logger::init().is_err() {
         panic!("Could not initiliaze logger");
     }
-    let args: Vec<_> = env::args().collect();
+    let cli_args = App::new("Dirac Host Properties Checker")
+                   .version(VERSION)
+                   .arg(Arg::with_name("check_suite")
+                            .takes_value(true)
+                            .value_name("FILE")
+                            .min_values(1)
+                            .help("Check suites to run"))
+                   .get_matches();
 
-    let check_suite = CheckSuite::read_from_file(&args[1]).unwrap();
+    let check_suite_filenames = cli_args.values_of("check_suite").unwrap();
+    for filename in check_suite_filenames {
+        run_check_suite(filename);
+    }
+}
 
+fn run_check_suite(file_name: &str) {
+    let check_suite = CheckSuite::read_from_file(file_name).unwrap();
     let results_by_host = dirac::engine::run(&check_suite);
 
     println!("SUMMARY");
