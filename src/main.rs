f87a644466c08e6479682536524aeb8fd09a530a
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-04-06 14:36:24 +0200

    Adds groundwork for Json Reporter

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ extern crate dirac;
 use clap::{Arg, ArgMatches, App};
 use std::collections::HashMap;
 use std::env;
-use term_painter::ToStyle;
-use term_painter::Color::*;
-use term_painter::Attr::*;
+
 
 use dirac::checks::CheckSuite;
 use dirac::engine::CheckSuiteResult;
@@ -24,21 +22,45 @@ fn main() {
         panic!("Could not initiliaze logger");
     }
     let cli_args = App::new("Dirac Host Properties Checker")
-                   .version(VERSION)
-                   .arg(Arg::with_name("check_suite")
-                            .takes_value(true)
-                            .value_name("FILE")
-                            .min_values(1)
-                            .help("Check suites to run"))
-                   .get_matches();
+                       .version(VERSION)
+                       .arg(Arg::with_name("report")
+                                .takes_value(true)
+                                .requires("output")
+                                .possible_values(&["json"])
+                                .short("r")
+                                .long("report")
+                                .value_name("REPORT")
+                                .help("Enables report"))
+                       .arg(Arg::with_name("output")
+                                .takes_value(true)
+                                .requires("report")
+                                .short("o")
+                                .long("output")
+                                .value_name("FILENAME")
+                                .help("Sets output file for report"))
+                       .arg(Arg::with_name("check_suite")
+                                .takes_value(true)
+                                .value_name("FILENAME")
+                                .min_values(1)
+                                .help("Check suites to run"))
+                       .get_matches();
 
     let check_suite_filenames = cli_args.values_of("check_suite").unwrap();
+
     for filename in check_suite_filenames {
         let check_suite = CheckSuite::read_from_file(&filename).unwrap();
         let results = dirac::engine::run(&check_suite);
 
         let report = SummaryReport::new(&results);
         report.print();
+
+        if cli_args.is_present("report") && cli_args.is_present("output") {
+            let report_type = cli_args.value_of("report").unwrap().to_string();
+            let report_filename = cli_args.value_of("output").unwrap().to_string();
+
+            let mut report_builder = Reporter::new(&results, &report_type);
+            let report = report_builder.with_filename(&report_filename).create();
+            report.print();
+        }
     }
 }
-
--- src/reports/mod.rs
+++ src/reports/mod.rs
@@ -5,36 +5,94 @@ use term_painter::Attr::*;
 
 use super::engine::CheckSuiteResult;
 
+pub struct Reporter<'a> {
+    check_suite_result: &'a CheckSuiteResult<'a>,
+    report_type: ReportType,
+    filename: Option<&'a str>,
+}
+
+impl<'a> Reporter<'a> {
+    pub fn new(check_suite_result: &'a CheckSuiteResult, report_type_str: &'a str) -> Reporter<'a> {
+        let report_type = match report_type_str {
+            "json" => ReportType::Json,
+            _ => panic!("Mööp"),
+        };
+        Reporter {
+            check_suite_result: check_suite_result,
+            report_type: report_type,
+            filename: None,
+        }
+    }
+
+    pub fn with_filename(&'a mut self, filename: &'a str) -> &'a mut Reporter<'a> {
+        self.filename = Some(filename);
+        self
+    }
+
+    pub fn create(&self) -> JsonReport<'a> {
+        match self.report_type {
+            ReportType::Json => JsonReport::new(self.check_suite_result, self.filename.unwrap()),
+        }
+    }
+}
+
+pub enum ReportType {
+    Json,
+}
+
 pub trait Report<'a> {
-    fn new(check_suite_result: &'a CheckSuiteResult) -> Self;
     fn print(&self);
 }
+
+pub struct JsonReport<'a> {
+    check_suite_result: &'a CheckSuiteResult<'a>,
+    filename: &'a str,
+}
+
+impl<'a> Report<'a> for JsonReport<'a> {
+    fn print(&self) {
+        println!("Mööp: Json Report");
+    }
+}
+
+impl<'a> JsonReport<'a> {
+    fn new(check_suite_result: &'a CheckSuiteResult, filename: &'a str) -> JsonReport<'a> {
+        JsonReport {
+            check_suite_result: check_suite_result,
+            filename: filename,
+        }
+    }
+}
+
 
 pub struct SummaryReport<'a> {
     check_suite_result: &'a CheckSuiteResult<'a>,
 }
 
 impl<'a> Report<'a> for SummaryReport<'a> {
-    fn new(check_suite_result: &'a CheckSuiteResult) -> SummaryReport<'a> {
-        SummaryReport { check_suite_result: check_suite_result }
-    }
-
     fn print(&self) {
         println!("{}", Bold.paint("SUMMARY"));
         let summary = SummaryReport::createSummary(self.check_suite_result);
         for kv in summary {
-            println!(" * {:<30} Success {:4}, Failed {:4}", kv.0, Green.paint((kv.1).0), Red.paint((kv.1).1));
+            println!(" * {:<30} Success {:4}, Failed {:4}",
+                     kv.0,
+                     Green.paint((kv.1).0),
+                     Red.paint((kv.1).1));
         }
     }
 }
 
 impl<'a> SummaryReport<'a> {
+    pub fn new(check_suite_result: &'a CheckSuiteResult) -> SummaryReport<'a> {
+        SummaryReport { check_suite_result: check_suite_result }
+    }
+
     fn createSummary(check_suite_result: &'a CheckSuiteResult) -> HashMap<&'a str, (u16, u16)> {
         let mut result = HashMap::new();
 
         for check in &check_suite_result.results {
             for property in &check.results {
-                let mut host_result = result.entry(property.host).or_insert((0,0));
+                let mut host_result = result.entry(property.host).or_insert((0, 0));
                 if property.result.is_ok() {
                     host_result.0 += 1;
                 } else {
