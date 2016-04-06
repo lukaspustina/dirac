dc119388d0f8e74a595abc581b3c6bf97aab9733
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-04-06 13:31:18 +0200

    Adds summary as first report

--- src/reports/mod.rs
+++ src/reports/mod.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+use term_painter::ToStyle;
+use term_painter::Color::*;
+use term_painter::Attr::*;
+
 use super::engine::CheckSuiteResult;
 
 pub trait Report<'a> {
@@ -15,7 +20,29 @@ impl<'a> Report<'a> for SummaryReport<'a> {
     }
 
     fn print(&self) {
-        println!("Mööp");
+        println!("{}", Bold.paint("SUMMARY"));
+        let summary = SummaryReport::createSummary(self.check_suite_result);
+        for kv in summary {
+            println!(" * {:<30} Success {:4}, Failed {:4}", kv.0, Green.paint((kv.1).0), Red.paint((kv.1).1));
+        }
     }
 }
 
+impl<'a> SummaryReport<'a> {
+    fn createSummary(check_suite_result: &'a CheckSuiteResult) -> HashMap<&'a str, (u16, u16)> {
+        let mut result = HashMap::new();
+
+        for check in &check_suite_result.results {
+            for property in &check.results {
+                let mut host_result = result.entry(property.host).or_insert((0,0));
+                if property.result.is_ok() {
+                    host_result.0 += 1;
+                } else {
+                    host_result.1 += 1;
+                }
+            }
+        }
+
+        result
+    }
+}
