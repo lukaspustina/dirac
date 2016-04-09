ad8a43d76475fbda8b84b34971dda67c44d3e70b
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-04-09 14:43:05 +0200

    Refactores summary printing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ extern crate term_painter;
 extern crate dirac;
 
 use clap::{Arg, App};
+use term_painter::ToStyle;
+use term_painter::Color::*;
+use term_painter::Attr::*;
+use std::collections::HashMap;
 
 use dirac::checks::CheckSuite;
-use dirac::reports::*;
+use dirac::engine::CheckSuiteResult;
+use dirac::reports::{Report, Reporter};
 
 static VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -46,9 +51,7 @@ fn main() {
     for filename in check_suite_filenames {
         let check_suite = CheckSuite::read_from_file(&filename).unwrap();
         let results = dirac::engine::run(&check_suite);
-
-        let report = SummaryReport::new(&results);
-        println!("{}", report.as_string());
+        print_summary(&results);
 
         if cli_args.is_present("report") && cli_args.is_present("output") {
             let report_type = cli_args.value_of("report").unwrap().to_string();
@@ -60,3 +63,33 @@ fn main() {
         }
     }
 }
+
+fn print_summary(check_suite_result: &CheckSuiteResult) {
+    let summary = create_summary(check_suite_result);
+
+    print!("{}\n", Bold.paint("SUMMARY"));
+    for kv in summary {
+        print!(" * {:<30} Success {:4}, Failed {:4}\n",
+                 kv.0,
+                 Green.paint((kv.1).0),
+                 Red.paint((kv.1).1));
+    }
+}
+
+fn create_summary<'a>(check_suite_result: &'a CheckSuiteResult) -> HashMap<&'a str, (u16, u16)> {
+    let mut result = HashMap::new();
+
+    for check in &check_suite_result.results {
+        for property in &check.results {
+            let mut host_result = result.entry(property.host).or_insert((0, 0));
+            if property.result.is_ok() {
+                host_result.0 += 1;
+            } else {
+                host_result.1 += 1;
+            }
+        }
+    }
+
+    result
+}
+
--- src/reports/mod.rs
+++ src/reports/mod.rs
@@ -1,14 +1,9 @@
 pub mod json;
 
-use std::collections::HashMap;
 use std::io;
-use term_painter::ToStyle;
-use term_painter::Color::*;
-use term_painter::Attr::*;
 
 use super::engine::*;
 use self::json::*;
-
 
 pub struct Reporter<'a> {
     check_suite_result: &'a CheckSuiteResult<'a>,
@@ -50,49 +45,3 @@ pub trait Report<'a> {
     fn write_to_file(&self) -> io::Result<()>;
 }
 
-pub struct SummaryReport<'a> {
-    check_suite_result: &'a CheckSuiteResult<'a>,
-}
-
-impl<'a> Report<'a> for SummaryReport<'a> {
-    // TODO: Farben werden nicht dargestellt
-    fn as_string(&self) -> String {
-        let mut s = String::new();
-        s.push_str(&format!("{}\n", Bold.paint("SUMMARY")));
-        let summary = SummaryReport::create_summary(self.check_suite_result);
-        for kv in summary {
-            s.push_str(&format!(" * {:<30} Success {:4}, Failed {:4}\n",
-                     kv.0,
-                     Green.paint((kv.1).0),
-                     Red.paint((kv.1).1)));
-        }
-        s
-    }
-
-    fn write_to_file(&self) -> io::Result<()> {
-        panic!("Not implemented");
-    }
-}
-
-impl<'a> SummaryReport<'a> {
-    pub fn new(check_suite_result: &'a CheckSuiteResult) -> SummaryReport<'a> {
-        SummaryReport { check_suite_result: check_suite_result }
-    }
-
-    fn create_summary(check_suite_result: &'a CheckSuiteResult) -> HashMap<&'a str, (u16, u16)> {
-        let mut result = HashMap::new();
-
-        for check in &check_suite_result.results {
-            for property in &check.results {
-                let mut host_result = result.entry(property.host).or_insert((0, 0));
-                if property.result.is_ok() {
-                    host_result.0 += 1;
-                } else {
-                    host_result.1 += 1;
-                }
-            }
-        }
-
-        result
-    }
-}
